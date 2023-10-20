a1c56a48c494afbb78b74c8d49d07ff0422a9b69
Hritam5002 <140381427+Hritam5002@users.noreply.github.com>
2023-10-20 20:11:03 +0530

    Update movies_scraper.py

--- movies_scraper.py
+++ movies_scraper.py
@@ -9,7 +9,7 @@ api_key = "d7d16a83219209d83e5748ef7a5407c37ca845f9"
 def search_movies(query):
     movies_list = []
     movies_details = {}
-    website = BeautifulSoup(requests.get(f"https://185.53.88.104/?s={query.replace(' ', '+')}").text, "html.parser")
+    website = BeautifulSoup(requests.get(f"https://skymovieshd.boo/?s={query.replace(' ', '+')}").text, "html.parser")
     movies = website.find_all("a", {'class': 'ml-mask jt'})
     for movie in movies:
         if movie:
